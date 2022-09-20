e0c40ff1e3827eb95b1475388a7938caf0a0e5e8
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-20 20:15:32 +0530

    Program to view Employee Payroll details in a Tabular Format from JSON Object

--- Home.js
+++ Home.js
@@ -1,6 +1,9 @@
+let empParrollList;
+
 window.addEventListener('DOMContentLoaded',(event)=>
 {
     createInnerHtml();
+    document.querySelector('.emp-count').textContent=empParrollList.length;
 });
 
 const createInnerHtml=()=>
@@ -8,20 +11,124 @@ const createInnerHtml=()=>
     const headerHTml="<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
     "<th>Salary</th><th>Start Date</th><th>Actions</th>";
 
-    const innerHtml=`${headerHTml}
+    let innerHtml=`${headerHTml}`;
+     empParrollList=createEmployeePayrollJSON();
+    for(const empParrollData of empParrollList)
+     { 
+
+        innerHtml=`${innerHtml}
+
+    
     <tr>
-    <td><img class = "profile" alt="" src="profile3.png"></td>
-    <td>Narayan</td>
-    <td>Male</td>
-    <td><div class="dept-label">HR</div>
-    <div class = "dept-label">Finance</div></td>
-    <td>30000</td>
-    <td>1 Nov 2020</td>
+    <td><img class = "profile" alt="" src=${empParrollData._profilePic}></td>
+    <td>${empParrollData._name}</td>
+    <td>${empParrollData._gender}</td>  
+    
+    <td>${getDepthtml(empParrollData._department)}</td>
+    
+    
+    
+    <td>${empParrollData._salary}</td>
+    <td>${empParrollData._startDate}</td>
     <td>
-        <button onclick = "remove(this)">Delete</button>
-        <button onclick = "edit(this)">Edit</button>   
+        <button name="${empParrollData._id} onclick = "remove(this)">Delete</button>
+        <button name="${empParrollData._id} onclick = "edit(this)">Edit</button>   
     </td>  
     </tr>
-    `;
+    `};
     document.querySelector('#display').innerHTML=innerHtml;
+     
 }
+
+const getDepthtml=(depList)=>
+{
+    let depHtml='';
+    for(const dept of depList)
+    {
+        depHtml=`${depHtml}<div class ='dept-label'>${dept}</div>`
+
+    }
+    return depHtml;
+}
+
+
+const createEmployeePayrollJSON=()=>
+{
+    let empPayrollListLocal=
+    [
+    {
+        _name: 'Milan',
+        _gender: 'Male',
+        _department: ['Engineering', ' ','Finance'],
+        _salary: '50000',
+        _startDate:'29 Oct 2019',
+        _note:'',
+        _id:new Date().getTime(),
+        _profilePic:'profile5.png'
+
+    },
+    {
+        _name: 'Chaitra',
+        _gender: 'female',
+        _department: ['HR' ,'Finance'],
+        _salary: '60000',
+        _startDate:'30 Oct 2019',
+        _note:'',
+        _id:new Date().getTime(),
+        _profilePic:'profile6.png'
+
+    },
+
+    {
+        _name: 'Sagar',
+        _gender: 'Male',
+        _department: ['HR' ,'Sales'],
+        _salary: '40000',
+        _startDate:'05 Oct 2019',
+        _note:'',
+        _id:new Date().getTime(),
+        _profilePic:'profile5.png'
+
+    },
+
+    {
+        _name: 'Megha',
+        _gender: 'female',
+        _department: ['HR' ,'Quality'],
+        _salary: '600000',
+        _startDate:'20 Nov 2020',
+        _note:'',
+        _id:new Date().getTime(),
+        _profilePic:'profile4.png'
+
+    },
+
+    {
+        _name: 'Rani',
+        _gender: 'female',
+        _department: ['Sales' ,'Finance'],
+        _salary: '60000',
+        _startDate:'15 Dec 2021',
+        _note:'',
+        _id:new Date().getTime(),
+        _profilePic:'profile6.png'
+
+    },
+
+    {
+        _name: 'Raj',
+        _gender: 'Male',
+        _department: ['HR' ,'Engineering'],
+        _salary: '80000',
+        _startDate:'30 Oct 2019',
+        _note:'',
+        _id:new Date().getTime(),
+        _profilePic:'profile5.png'
+
+    }
+
+];
+return empPayrollListLocal;
+
+}
+
